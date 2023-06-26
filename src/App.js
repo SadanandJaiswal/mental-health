936c5c30b6f8a4151434144462a14fc7e95dded5
Kiran <gkirankumar503@gmail.com>
2023-06-26 19:05:20 +0530

    Side bar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Meditaion from "./pages/Meditaion";
+import Music from "./pages/Music";
+import Profile from "./pages/Profile";
+import Sleep from "./pages/Sleep";
+import Affirmation from "./pages/Affirmation";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Sidebar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/meditation" element={<Meditaion />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sleep" element={<Sleep />} />
+          <Route path="/affirmation" element={<Affirmation />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+        </Sidebar>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Sleep.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sleep = () => {
+  return (
+    <div>
+      <h1> Sleep </h1>
+    </div>
+  )
+}
+
+export default Sleep
